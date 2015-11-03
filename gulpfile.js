49bae4b76a6558fd316a3b08e9396d9d1739397e
Pavlo <pavlo.andriiesh@gmail.com>
2015-11-04 00:05:38 +0200

    Updating to v.0.0.7

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,15 @@ gulp.task('compress', function() {
         .pipe(uglify())
         .pipe(gulp.dest('tmp/'));
 });
-
 gulp.task('rename', ['copy', 'compress'], function() {
     return gulp.src("tmp/translit.js")
         .pipe(rename("translit.min.js"))
         .pipe(gulp.dest("./dist"));
 });
-
 gulp.task('clean', ['rename'], function () {
     return gulp.src('tmp', {read: false})
         .pipe(clean());
 });
-
 gulp.task('default', ['clean'], function() {
 
 });
